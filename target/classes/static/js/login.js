2a2577f74e9ec084d5ec979d2b083adfe3039d27
Rakhim <rahimbekabdumomunov@gmail.com>
2024-04-23 18:10:38 +0300

    delete target dir

--- target/classes/static/js/login.js
+++
@@ -1,8 +0,0 @@
-const passwordInput = document.getElementById('password');
-const showPasswordCheckbox = document.getElementById('showPassword');
-showPasswordCheckbox.addEventListener('change', function() {
-    if (showPasswordCheckbox.checked) {
-        passwordInput.type = 'text';
-    } else {
-        passwordInput.type = 'password';
-    }});
